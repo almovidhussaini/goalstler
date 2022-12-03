682f481764e321fa603fa811aeb26c82bca20086
almovidhussaini <93880441+almovidhussaini@users.noreply.github.com>
2022-12-03 23:47:52 +0500

    sss

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use('/api/user', require('./routes/userRoutes'))
 
 if(process.env.NODE_ENV=='production'){
 // app.use(express.static('build'))
-app.use(express.static(path.resolve(__dirname, '/build')));
+app.use(express.static(path.resolve(__dirname, './build')));
 // app.get('*',(req,res)=>res.sendFile(
 //         path.resolve(__dirname,'build','index.html')
 //     ));
