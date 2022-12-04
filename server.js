1a4ec54e63a036cd2d0146f83e7eda07cbbe3ace
almovidhussaini <93880441+almovidhussaini@users.noreply.github.com>
2022-12-04 19:18:43 +0500

    f55

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use('/api/user', require('./routes/userRoutes'))
 
 if(process.env.NODE_ENV=='production'){
 // app.use(express.static('build'))
-app.use(express.static(path.resolve(__dirname, './buildd')));
+app.use(express.static(path.resolve(__dirname, './build')));
 // app.get('*',(req,res)=>res.sendFile(
 //         path.resolve(__dirname,'build','index.html')
 //     ));
@@ -30,7 +30,7 @@ app.use(express.static(path.resolve(__dirname, './buildd')));
 //     res.send(__dirname)
 // })
      app.get('*', (req, res) => {
-     res.sendFile(path.resolve(__dirname, 'buildd', 'index.html'));
+     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
     });
 }
 else{
